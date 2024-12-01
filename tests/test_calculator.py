5a8c51ff784be95e1feabe4ab948dfef468b3718
“saranya_mlops” <saran.msm@gmail.com>
2024-12-01 10:32:57 +0100

    parent directory change

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,4 +1,12 @@
 import pytest
+import os
+import sys
+
+# Add the parent directory to the Python path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+
 from calculator.calculator import Calculator
 import math
 
